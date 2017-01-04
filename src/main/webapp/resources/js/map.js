00ccaeff30cf436f123afb6b58e8fa3bfe7f710d
Arakadiusz Aptewicz <Arkadiusz.Aptewicz@dbschenker.com>
2017-01-04 16:16:34 +0100

    markers for nodes

--- src/main/webapp/resources/js/map.js
+++ src/main/webapp/resources/js/map.js
@@ -7,11 +7,25 @@ $(document).ready(function(){
             .done(function (msg) {
                 var issueLocation;
                 for(var i = 0; i < msg.length; i++) {
-                    issueLocation = {lat: msg[0].latitude, lng: msg[0].longitude};
+                    issueLocation = {lat: msg[i].latitude, lng: msg[i].longitude};
                      var marker = new google.maps.Marker({
                         position: issueLocation,
                         map: map
                     });
+                    for(var j = 0; j < msg[i].ftthJob.affectedEdges.length; j++) {
+                        var affectedEdgeNodeA = msg[i].ftthJob.affectedEdges[j].nodeA;
+                        var affectedEdgeNodeB = msg[i].ftthJob.affectedEdges[j].nodeB;
+                        var nodeA = {lat: affectedEdgeNodeA.y, lng: affectedEdgeNodeA.x};
+                        var nodeB = {lat: affectedEdgeNodeB.y, lng: affectedEdgeNodeB.x};
+                        var markerNodeA = new google.maps.Marker({
+                            position: nodeA,
+                            map: map
+                        });
+                        var markerNodeB = new google.maps.Marker({
+                            position: nodeB,
+                            map: map
+                        });
+                    }
                 }
                 map.setZoom(15);
                 map.setCenter(new google.maps.LatLng(issueLocation.lat, issueLocation.lng));
